a49ea030aa2378ed454d5c9353d1bc2907d04ea2
Gabriel Fabian <gabrielferreirafabian@gmail.com>
2022-07-07 21:24:27 -0300

    feat: ensure Login calls Validation with correct email

--- src/presentation/components/form-status/form-status.tsx
+++ src/presentation/components/form-status/form-status.tsx
@@ -4,12 +4,12 @@ import { Spinner } from '@/presentation/components'
 import Context from '@/presentation/contexts/form/form-context'
 
 const FormStatus: React.FC = () => {
-  const { state, errorState } = useContext(Context)
+  const { state } = useContext(Context)
 
   return (
     <div data-testid="error-wrap" className={Styles.errorWrap}>
       { state.isLoading && <Spinner className={Styles.spinner}/> }
-      { errorState.message && <span className={Styles.error}>{errorState.message}</span> }
+      { state.messageError && <span className={Styles.error}>{state.messageError}</span> }
     </div>
   )
 }
