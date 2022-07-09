b58e99cec31862f36b4cec7305e47acd5496594c
Gabriel Fabian <gabrielferreirafabian@gmail.com>
2022-07-09 20:16:32 -0300

    chore: add files to coverage ignore

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,9 @@ module.exports = {
   roots: ['<rootDir>/src'],
   collectCoverageFrom: [
     '<rootDir>/src/**/*.{ts,tsx}',
+    '!<rootDir>/src/presentation/components/router/**/*',
+    '!<rootDir>/src/main/**/*',
+    '!<rootDir>/src/**/index.ts',
     '!**/*.d.ts'
   ],
   coverageDirectory: 'coverage',
