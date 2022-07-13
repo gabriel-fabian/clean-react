4df360db23f589e5404f42e4fbf2d35d03a0166c
Gabriel Fabian <gabrielferreirafabian@gmail.com>
2022-07-12 23:01:20 -0300

    test: add SignUpValidationFactory

---
+++ src/main/factories/pages/signup/signup-validation-factory.spec.ts
@@ -0,0 +1,15 @@
+import { makeSignUpValidation } from './signup-validation-factory'
+import { ValidationBuilder as Builder } from '@/validation/validators/builder/validation-builder'
+import { ValidationComposite } from '@/validation/validators'
+
+describe('SignUpValidationFactory', () => {
+  test('Should make ValidationComposite with correct validations', () => {
+    const composite = makeSignUpValidation()
+    expect(composite).toEqual(ValidationComposite.build([
+      ...Builder.field('name').required().minLength(2).build(),
+      ...Builder.field('email').required().email().build(),
+      ...Builder.field('password').required().minLength(5).build(),
+      ...Builder.field('passwordConfirmation').required().minLength(5).build()
+    ]))
+  })
+})
