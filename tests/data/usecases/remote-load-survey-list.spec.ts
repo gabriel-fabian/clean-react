1c3fa333153c26fc1c8e110163c8ce983930b660
Gabriel Fabian <gabrielferreirafabian@gmail.com>
2022-07-13 22:09:25 -0300

    test: ensure RemoteLoadSurveyList throws UnexpectedError on 404

--- tests/data/usecases/remote-load-survey-list.spec.ts
+++ tests/data/usecases/remote-load-survey-list.spec.ts
@@ -36,4 +36,13 @@ describe('RemoteLoadSurveyList', () => {
     const promise = sut.loadAll()
     await expect(promise).rejects.toThrow(new UnexpectedError())
   })
+
+  test('Throw UnexpectedError if HttpGetClient returns 404', async () => {
+    const { sut, httpGetClientSpy } = makeSut()
+    httpGetClientSpy.response = {
+      statusCode: HttpStatusCode.notFound
+    }
+    const promise = sut.loadAll()
+    await expect(promise).rejects.toThrow(new UnexpectedError())
+  })
 })
