05bb11c84ea125e73b3fd87f97918cf3ecc86347
Gabriel Fabian <gabrielferreirafabian@gmail.com>
2022-07-13 21:53:02 -0300

    feat: ensure RemoteLoadSurveyList calls HttpGetClient with correct URL

--- src/data/usecases/index.ts
+++ src/data/usecases/index.ts
@@ -1,3 +1,4 @@
 export * from './add-account/remote-add-account'
 export * from './authentication/remote-authentication'
 export * from './save-access-token/local-save-access-token'
+export * from './load-survey-list/remote-load-survey-list'
