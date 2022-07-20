1a0bc043a3214518f766079ceb277c8ba2f94d0b
Gabriel Fabian <gabrielferreirafabian@gmail.com>
2022-07-20 18:56:53 -0300

    test: ensure CurrentAccountAdapter throws if an invalid account is provided

--- tests/main/adapters/current-account-adapter.spec.ts
+++ tests/main/adapters/current-account-adapter.spec.ts
@@ -1,3 +1,4 @@
+import { UnexpectedError } from '@/domain/errors'
 import { LocalStorageAdapter } from '@/infra/cache/local-storage-adapter'
 import { setCurrentAccountAdapter } from '@/main/adapters/current-account-adapter'
 import { mockAccountModel } from '@/tests/domain/mocks'
@@ -10,5 +11,11 @@ describe('CurrentAccountAdapter', () => {
     const setSpy = jest.spyOn(LocalStorageAdapter.prototype, 'set')
     setCurrentAccountAdapter(account)
     expect(setSpy).toHaveBeenCalledWith('account', account)
+  })
+
+  test('Should thow UnexpectedError', () => {
+    expect(() => {
+      setCurrentAccountAdapter(undefined)
+    }).toThrow(new UnexpectedError())
   })
 })
