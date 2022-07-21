6f148607070c49ac0fef8645214f3eae9b057973
Gabriel Fabian <gabrielferreirafabian@gmail.com>
2022-07-20 21:58:28 -0300

    feat: apply PrivateRoute to SurveyList page

--- src/main/routes/router.tsx
+++ src/main/routes/router.tsx
@@ -5,6 +5,7 @@ import { MakeLogin } from '@/main/factories/pages/login/login-factory'
 import { MakeSignUp } from '@/main/factories/pages/signup/signup-factory'
 import { ApiContext } from '@/presentation/contexts/'
 import { SurveyList } from '@/presentation/pages'
+import { PrivateRoute } from '@/presentation/components'
 
 const Router: React.FC = () => {
   return (
@@ -17,7 +18,7 @@ const Router: React.FC = () => {
         <Routes>
           <Route path="/login" element={<MakeLogin />} />
           <Route path="/signup" element={<MakeSignUp />} />
-          <Route path="/" element={<SurveyList />} />
+          <Route path="/" element={<PrivateRoute><SurveyList /></PrivateRoute>} />
         </Routes>
       </BrowserRouter>
     </ApiContext.Provider>
