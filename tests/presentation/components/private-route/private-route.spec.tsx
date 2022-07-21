f3c5eedebf458dd48504a6f66b6d2b11bdb12037
Gabriel Fabian <gabrielferreirafabian@gmail.com>
2022-07-20 21:53:40 -0300

    test: ensure PrivateRoute renders component if token is not empty

--- tests/presentation/components/private-route/private-route.spec.tsx
+++ tests/presentation/components/private-route/private-route.spec.tsx
@@ -3,17 +3,30 @@ import { PrivateRoute } from '@/presentation/components'
 import { render } from '@testing-library/react'
 import { createMemoryHistory, MemoryHistory } from 'history'
 import { Router } from 'react-router-dom'
+import { ApiContext } from '@/presentation/contexts'
+import { mockAccountModel } from '@/tests/domain/mocks'
+import { SignUp } from '@/presentation/pages'
+import { AddAccountSpy, ValidationStub } from '@/tests/presentation/mocks'
 
 type SutTypes = {
   history: MemoryHistory
 }
 
-const makeSut = (): SutTypes => {
+const makeSut = (account = mockAccountModel()): SutTypes => {
   const history = createMemoryHistory({ initialEntries: ['/'] })
+  const validationStub = new ValidationStub()
+  const addAccountSpy = new AddAccountSpy()
   render(
-    <Router location={history.location} navigator={history}>
-      <PrivateRoute />
-    </Router>
+    <ApiContext.Provider value={{ getCurrentAccount: () => account }}>
+      <Router location={history.location} navigator={history}>
+          <PrivateRoute>
+            <SignUp
+              addAccount={addAccountSpy}
+              validation={validationStub}
+            />
+          </PrivateRoute>
+      </Router>
+    </ApiContext.Provider>
   )
   return {
     history
@@ -22,7 +35,12 @@ const makeSut = (): SutTypes => {
 
 describe('PrivateRoute', () => {
   test('Should redirect to /login if accessToken is empty', () => {
-    const { history } = makeSut()
+    const { history } = makeSut(null)
     expect(history.location.pathname).toBe('/login')
+  })
+
+  test('Should render current component if accessToken is not empty', () => {
+    const { history } = makeSut()
+    expect(history.location.pathname).toBe('/')
   })
 })
