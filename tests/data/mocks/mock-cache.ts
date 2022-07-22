22ccf4cdf8c9bcd165518a3753d0295be3f43b6f
Gabriel Fabian <gabrielferreirafabian@gmail.com>
2022-07-22 18:17:27 -0300

    test: ensure AuthorizeHttpGetClientDecorator call GetStorage with correct value

--- tests/data/mocks/mock-cache.ts
+++ tests/data/mocks/mock-cache.ts
@@ -1,11 +1,12 @@
-import { SetStorage } from '@/data/protocols/cache'
+import { GetStorage } from '@/data/protocols/cache'
+import { faker } from '@faker-js/faker'
 
-export class SetStorageMock implements SetStorage {
+export class GetStorageSpy implements GetStorage {
   key: string
-  value: string
+  value = faker.datatype.json()
 
-  set (key: string, value: any): void {
+  get (key: string): any {
     this.key = key
-    this.value = value
+    return this.value
   }
 }
