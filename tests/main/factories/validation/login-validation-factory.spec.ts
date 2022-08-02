68a85b9dddfdc5343f6ff1a7f56c0f8c8f1aaa01
Gabriel Fabian <gabrielferreirafabian@gmail.com>
2022-08-02 17:31:03 -0300

    refactor: change validation factory to not use builder

--- tests/main/factories/validation/login-validation-factory.spec.ts
+++ tests/main/factories/validation/login-validation-factory.spec.ts
@@ -1,13 +1,19 @@
 import { makeLoginValidation } from '@/main/factories/pages/login/login-validation-factory'
-import { ValidationBuilder as Builder } from '@/validation/validators/builder/validation-builder'
-import { ValidationComposite } from '@/validation/validators'
+import {
+  EmailValidation,
+  MinLenghtValidation,
+  RequiredFieldValidation,
+  ValidationComposite
+} from '@/validation/validators'
 
 describe('LoginValidationFactory', () => {
   test('Should make ValidationComposite with correct validations', () => {
     const composite = makeLoginValidation()
     expect(composite).toEqual(ValidationComposite.build([
-      ...Builder.field('email').required().email().build(),
-      ...Builder.field('password').required().minLength(5).build()
+      new RequiredFieldValidation('email'),
+      new EmailValidation('email'),
+      new RequiredFieldValidation('password'),
+      new MinLenghtValidation('password', 5)
     ]))
   })
 })
--- tests/main/factories/validation/signup-validation-factory.spec.ts
+++ tests/main/factories/validation/signup-validation-factory.spec.ts
@@ -1,15 +1,24 @@
 import { makeSignUpValidation } from '@/main/factories/pages/signup/signup-validation-factory'
-import { ValidationBuilder as Builder } from '@/validation/validators/builder/validation-builder'
-import { ValidationComposite } from '@/validation/validators'
+import {
+  CompareFieldsValidation,
+  EmailValidation,
+  MinLenghtValidation,
+  RequiredFieldValidation,
+  ValidationComposite
+} from '@/validation/validators'
 
 describe('SignUpValidationFactory', () => {
   test('Should make ValidationComposite with correct validations', () => {
     const composite = makeSignUpValidation()
     expect(composite).toEqual(ValidationComposite.build([
-      ...Builder.field('name').required().minLength(2).build(),
-      ...Builder.field('email').required().email().build(),
-      ...Builder.field('password').required().minLength(5).build(),
-      ...Builder.field('passwordConfirmation').required().equalTo('password').build()
+      new RequiredFieldValidation('name'),
+      new MinLenghtValidation('name', 2),
+      new RequiredFieldValidation('email'),
+      new EmailValidation('email'),
+      new RequiredFieldValidation('password'),
+      new MinLenghtValidation('password', 5),
+      new RequiredFieldValidation('passwordConfirmation'),
+      new CompareFieldsValidation('passwordConfirmation', 'password')
     ]))
   })
 })
