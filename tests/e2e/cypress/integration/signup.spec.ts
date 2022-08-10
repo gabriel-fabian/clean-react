93ef1924c91658afa222324246e2bfb8385ec851
Gabriel Fabian <gabrielferreirafabian@gmail.com>
2022-08-10 20:25:05 -0300

    test: ensure Signup loads with correct initial state

---
+++ tests/e2e/cypress/integration/signup.spec.ts
@@ -0,0 +1,16 @@
+import * as FormHelper from '../support/form-helper'
+
+describe('SignUp', () => {
+  beforeEach(() => {
+    cy.visit('signup')
+  })
+
+  it('Should load with correct initial state', () => {
+    FormHelper.testInputStatus('name', 'Campo obrigatório')
+    FormHelper.testInputStatus('email', 'Campo obrigatório')
+    FormHelper.testInputStatus('password', 'Campo obrigatório')
+    FormHelper.testInputStatus('passwordConfirmation', 'Campo obrigatório')
+    cy.getByTestId('submit').should('have.attr', 'disabled')
+    cy.getByTestId('error-wrap').should('not.have.descendants')
+  })
+})
