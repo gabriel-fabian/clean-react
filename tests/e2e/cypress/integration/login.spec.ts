64eed1a0b8fbd85120c57f6b0e4ccd9808c30bab
Gabriel Fabian <gabrielferreirafabian@gmail.com>
2022-08-10 18:27:30 -0300

    test: ensure Login will not submit form if is invalid

--- tests/e2e/cypress/integration/login.spec.ts
+++ tests/e2e/cypress/integration/login.spec.ts
@@ -122,4 +122,22 @@ describe('Login', () => {
     cy.getByTestId('submit').dblclick()
     cy.get('@request.all').should('have.length', 1)
   })
+
+  it('Should not call submit if form is invalid', () => {
+    cy.intercept({
+      method: 'POST',
+      url: /login/
+    }, {
+      statusCode: 200,
+      body: {
+        account: {
+          name: faker.name.findName(),
+          accessToken: faker.datatype.uuid()
+        }
+      }
+    }).as('request')
+
+    cy.getByTestId('email').type(faker.internet.email()).type('{enter}')
+    cy.get('@request.all').should('have.length', 0)
+  })
 })
