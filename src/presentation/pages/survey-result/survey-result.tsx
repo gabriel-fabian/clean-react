c84b59228c2056a3b5e64128736a36980dd9516c
Gabriel Fabian <gabrielferreirafabian@gmail.com>
2022-08-18 18:41:53 -0300

    feat: add skeleton with shimmer on SurveyResult

--- src/presentation/pages/survey-result/survey-result.tsx
+++ src/presentation/pages/survey-result/survey-result.tsx
@@ -7,29 +7,33 @@ const SurveyResult: React.FC = () => {
     <div className={Styles.surveyResultWrap}>
       <Header />
       <div className={Styles.contentWrap}>
-        <hgroup>
-          <Calendar date={new Date()} className={Styles.calendarWrap}/>
-          <h2>Qual é o seu framework web favorito?</h2>
-        </hgroup>
-        <ul>
-          <li>
-            <img src="https://fordevs.herokuapp.com/static/img/logo-react.png" />
-            <span className={Styles.answer}>ReactJS</span>
-            <span className={Styles.percent}>50%</span>
-          </li>
-          <li className={Styles.active}>
-            <img src="https://fordevs.herokuapp.com/static/img/logo-react.png" />
-            <span className={Styles.answer}>ReactJS</span>
-            <span className={Styles.percent}>50%</span>
-          </li>
-          <li>
-            <img src="https://fordevs.herokuapp.com/static/img/logo-react.png" />
-            <span className={Styles.answer}>ReactJS</span>
-            <span className={Styles.percent}>50%</span>
-          </li>
-        </ul>
-        <button>Voltar</button>
-        {false && <Loading />}
+        {true &&
+          <>
+          <hgroup>
+            <Calendar date={new Date()} className={Styles.calendarWrap}/>
+            <h2>Qual é o seu framework web favorito?</h2>
+          </hgroup>
+          <ul>
+            <li>
+              <img src="https://fordevs.herokuapp.com/static/img/logo-react.png" />
+              <span className={Styles.answer}>ReactJS</span>
+              <span className={Styles.percent}>50%</span>
+            </li>
+            <li className={Styles.active}>
+              <img src="https://fordevs.herokuapp.com/static/img/logo-react.png" />
+              <span className={Styles.answer}>ReactJS</span>
+              <span className={Styles.percent}>50%</span>
+            </li>
+            <li>
+              <img src="https://fordevs.herokuapp.com/static/img/logo-react.png" />
+              <span className={Styles.answer}>ReactJS</span>
+              <span className={Styles.percent}>50%</span>
+            </li>
+          </ul>
+          <button>Voltar</button>
+          {false && <Loading />}
+          </>
+        }
       </div>
       <Footer />
     </div>
