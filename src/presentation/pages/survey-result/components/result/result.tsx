d74aca92ffd40b6bf29b84eeef6f9c4ff467d275
Gabriel Fabian <gabrielferreirafabian@gmail.com>
2022-08-25 15:06:56 -0300

    refactor: move SurveyResultAnswer to its own component

--- src/presentation/pages/survey-result/components/result/result.tsx
+++ src/presentation/pages/survey-result/components/result/result.tsx
@@ -1,8 +1,9 @@
 import Styles from './result-styles.scss'
 import { Calendar } from '@/presentation/components'
+import { SurveyResultAnswer } from '@/presentation/pages/survey-result/components'
+import { LoadSurveyResult } from '@/domain/usecases'
 import { useNavigate } from 'react-router-dom'
 import React from 'react'
-import { LoadSurveyResult } from '@/domain/usecases'
 
 type Props = {
   surveyResult: LoadSurveyResult.Model
@@ -17,13 +18,7 @@ const Result: React.FC<Props> = ({ surveyResult }: Props) => {
         <h2 data-testid="question">{surveyResult.question}</h2>
       </hgroup>
       <ul data-testid="answers" className={Styles.answerList}>
-        {surveyResult.answers.map(answer =>
-          <li data-testid="answer-wrap" key={answer.answer} className={answer.isCurrentAccountAnswer ? Styles.active : ' '}>
-            {answer.image && <img data-testid="image" src={answer.image} alt={answer.answer} />}
-            <span data-testid="answer" className={Styles.answer}>{answer.answer}</span>
-            <span data-testid="percent" className={Styles.percent}>{answer.percent}%</span>
-          </li>
-        )}
+        {surveyResult.answers.map(answer => <SurveyResultAnswer key={answer.answer} answer={answer} />)}
       </ul>
       <button data-testid="back-button" className={Styles.button} onClick={() => navigate(-1)}>Voltar</button>
     </>
