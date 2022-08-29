cc4010b72c9fa0993e3a213a2b39be723d168a3c
Gabriel Fabian <gabrielferreirafabian@gmail.com>
2022-08-29 18:18:40 -0300

    test: ensure RemoteSaveSurveyResult returns SurveyResult on 200

--- tests/data/usecases/remote-save-survey-result.spec.ts
+++ tests/data/usecases/remote-save-survey-result.spec.ts
@@ -61,4 +61,19 @@ describe('RemoteSaveSurveyResult', () => {
     const promise = sut.save(mockSaveSurveyResultParams())
     await expect(promise).rejects.toThrow(new UnexpectedError())
   })
+
+  test('Should return a SurveyResult on 200', async () => {
+    const { sut, httpGetClientSpy } = makeSut()
+    const httpResult = mockRemoteSurveyResultModel()
+    httpGetClientSpy.response = {
+      statusCode: HttpStatusCode.ok,
+      body: httpResult
+    }
+    const httpResponse = await sut.save(mockSaveSurveyResultParams())
+    expect(httpResponse).toEqual({
+      question: httpResult.question,
+      answers: httpResult.answers,
+      date: new Date(httpResult.date)
+    })
+  })
 })
